352674cc0bf582a767b735e9a0cd78dca13738bc
Mateusz Połeć <polec0@wp.pl>
2019-12-17 15:01:20 +0100

    Lista 7

---
+++ lista6/baza5/index.php
@@ -0,0 +1,8 @@
+<head>
+    <title>Najlepsi uczniowie</title>
+    <link rel = "stylesheet" href = "style.css"/>
+</head>
+
+<?php
+    include 'table_display.php';
+?>
---
+++ lista6/baza5/table_connection.php
@@ -0,0 +1,3 @@
+<?php
+
+$connection = new SQLite3('baza.db');
